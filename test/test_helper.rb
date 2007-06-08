f5afaa404aa1e13e39e40915d16f2bd28f6d0349
tsaleh <tsaleh@7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa>
2007-06-08 18:30:23 +0000

    removed breakpoint
    git-svn-id: https://svn.thoughtbot.com/plugins/squirrel/trunk@121 7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ require 'rubygems'
 require 'test/unit'
 require 'active_record'
 require 'active_record/fixtures'
-require 'breakpoint'
 
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
